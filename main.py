c16227b5d95951004c1bee475f9fef7ae5c6f46e
jakubrejmak <jakubrejmak@gmail.com>
2025-06-16 20:34:17 +0200

    added quit event handling

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill((0, 0, 0))
         pygame.display.flip()
 
