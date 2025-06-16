5b69b53df51dd95b1b1290655b2d1cac5d6f4ef7
jakubrejmak <jakubrejmak@gmail.com>
2025-06-16 21:19:53 +0200

    added player object and got it to render on the screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 
 from constants import *
+from player import *
+
 
 def main():
     print("Starting Asteroids!")
@@ -10,6 +12,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # game loop
     while True:
         for event in pygame.event.get():
@@ -17,8 +24,12 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
+        # tick returns ms since last call. Converting this value to seconds and
+        # saving it in dt (delta time)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
